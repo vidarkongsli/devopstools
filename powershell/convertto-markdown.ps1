1dfc693f0deb19103e78bffc1c16cafe038cd02f
Vidar Kongsli <vidar@kongs.li>
2020-05-11 14:59:16 +0200

    Adjust markdown

--- powershell/convertto-markdown.ps1
+++ powershell/convertto-markdown.ps1
@@ -5,7 +5,7 @@ param(
 $workitems `
     | Group-Object -Property { $_.fields.'System.WorkItemType' } `
     | ForEach-Object {
-        ".h2 $($_.Name)"
+        "## $($_.Name)"
         $_.Group | ForEach-Object {
             "* [$($_.fields.'System.Title')]($($_._links.html.href)) ($($_.fields.'System.State'))"
         }
