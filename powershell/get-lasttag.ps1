f8a2051c97c86b7b1b52a3b8c59e68a2d41dbc8c
Vidar Kongsli <vidar@kongs.li>
2021-05-12 17:10:59 +0200

    Adjstments

--- powershell/get-lasttag.ps1
+++ powershell/get-lasttag.ps1
@@ -12,7 +12,6 @@ param(
 $auth = [Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes(":$pat"))
 $headers = @{'Authorization'="Basic $auth"}
 
-$result = Invoke-RestMethod `
+Invoke-RestMethod `
     -Uri "https://dev.azure.com/$organization/$project/_apis/git/repositories/$repositoryId/refs?api-version=5.1&filter=tags/v" `
-    -Headers $headers
-$result.value | sort-object -Property name -Descending | Select-Object -First 1
+    -Headers $headers | Select-Object -ExpandProperty value
